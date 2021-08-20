46ed969fe019c79b8341e770690e4a2586b686bd
Henrique <hentt30@gmail.com>
2021-08-20 10:45:47 -0300

    Adding styled components and creating Home page

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Component} from 'react';
+import {Title} from './styles';
+
+class Home extends Component {
+  render(): JSX.Element {
+    return (
+      <div>
+        <Title>My first home page</Title>
+      </div>
+    );
+  }
+}
+
+export default Home;
