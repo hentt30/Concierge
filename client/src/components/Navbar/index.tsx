489d252031d4e0c3ee26c6956035ef1d92c01cdc
Henrique <hentt30@gmail.com>
2021-08-27 08:35:44 -0300

    Styling navbar and adding sections

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import {Nav, NavbarContainer, NavLogo} from './styles';
+import {FaBars} from 'react-icons/fa';
+import {Nav, NavbarContainer, NavLogo, MobileIcon,
+  NavMenu, NavItem, NavLink} from './styles';
 const Navbar: React.FC = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/"> Concierge </NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLink to="about"> About </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="discover"> Discover </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="services"> Services </NavLink>
+            </NavItem>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
     </>
