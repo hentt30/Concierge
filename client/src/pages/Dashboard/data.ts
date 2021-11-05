a49215370478f6daed2172ecfc6c49ba5d2e0828
Henrique <hentt30@gmail.com>
2021-11-05 20:09:07 -0300

    Iniciando dashboard table

---
+++ client/src/pages/Dashboard/data.ts
@@ -0,0 +1,42 @@
+export const columns = [
+  {field: 'id', headerName: 'ID', width: 90},
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params):string =>
+      `${params.getValue(params.id, 'firstName') || ''} ${
+        params.getValue(params.id, 'lastName') || ''
+      }`,
+  },
+];
+
+export const rows = [
+  {id: 1, lastName: 'Snow', firstName: 'Jon', age: 35},
+  {id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42},
+  {id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45},
+  {id: 4, lastName: 'Stark', firstName: 'Arya', age: 16},
+  {id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 30},
+];
+
--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Dashbar from '../../components/Dashbar';
+import Table from '../../components/Table';
+import {rows, columns} from './data';
 
 const Dashboard: React.FC = ()=>{
   return (
     <div>
-      <Dashbar/>
-      <p>Meu dashboard</p>
+
+      <Table rowsData={rows} columnsData={columns}/>
     </div>
   );
 };
