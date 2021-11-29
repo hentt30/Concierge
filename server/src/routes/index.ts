e6a824d67c321702d285f52f81f0057a413d292f
Henrique <hentt30@gmail.com>
2021-11-29 15:01:32 -0300

    moving files

--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import auth from './auth';
-import playlist from './playlist';
-const routes = Router();
-
-routes.use('/auth', auth);
-routes.use('/playlist', playlist);
-
-export default routes;
--- server/src/routes/playlist.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import {checkJwt} from 'src/middlewares/checkJwt';
-import PlaylistController from '../controllers/PlaylistController';
-
-const router = Router();
-// Login route
-router.post('/create', [checkJwt], PlaylistController.createNewPlaylist);
-router.get('/getAll', [checkJwt], PlaylistController.getAllPlaylists);
-export default router;
