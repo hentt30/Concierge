d6661fd5e0feb6dedd71afb8c54119f2b9411e0c
Henrique <hentt30@gmail.com>
2021-11-29 16:59:44 -0300

    ultimos detalhes

--- client/src/components/HeroSection/index.tsx
+++ client/src/components/HeroSection/index.tsx
@@ -1,12 +1,12 @@
 
 import React, {useState} from 'react';
-import Video from '../../assets/video.mp4';
 import {Button} from '../Button/styles';
 import {ISpotify} from '../../services/spotify';
 import {HeroContainer, HeroBg, VideoBg,
   HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowFoward
   , ArrowRight} from './styles';
 
+const videoSrc = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/45bb965d-9175-461d-9163-bd28383e55bf/video.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211129%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211129T195854Z&X-Amz-Expires=86400&X-Amz-Signature=a8d1e6a3ddd7ded5adb646e4bd555d252eb68042fe6ca86dec2426f7c5986734&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22video.mp4%22&x-id=GetObject";
 type HeroSectionProps ={
     spotifyApi: ISpotify;
 }
@@ -21,7 +21,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({spotifyApi}) => {
   return (
     <HeroContainer id={'home'} >
       <HeroBg>
-        <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+        <VideoBg autoPlay loop muted src={videoSrc} type='video/mp4'/>
       </HeroBg>
       <HeroContent>
         <HeroH1>Get Out Of Your Comfort Zone</HeroH1>
--- client/src/components/infoSection/data.ts
+++ client/src/components/infoSection/data.ts
@@ -1,6 +1,4 @@
-import img from '../../assets/music.svg';
-import img2 from '../../assets/svg-2.svg';
-import img3 from '../../assets/svg-3.svg';
+
 export const homeObjOne = {
   id: 'about',
   lightBg: true,
@@ -14,7 +12,7 @@ export const homeObjOne = {
     to create a platform on which we could achieve our goals.`,
   buttonLabel: 'Get started',
   imgStart: false,
-  img: img,
+  img: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/67e138af-7212-46da-8e22-7e54983b0a4c/music.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211129%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211129T195155Z&X-Amz-Expires=86400&X-Amz-Signature=eb973f201a769a6502bd83bd06c98c5b63839c3b74a2e6f051aaa4b99a603cf6&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22music.svg%22&x-id=GetObject",
   alt: 'Music',
   darkText: true,
 };
@@ -30,7 +28,7 @@ export const homeObjTwo = {
   you can find new music and have a fun time.`,
   buttonLabel: 'Get started',
   imgStart: false,
-  img: img2,
+  img: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/bdb03a85-3688-4891-b96c-7aace00196d7/svg-2.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211129%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211129T195542Z&X-Amz-Expires=86400&X-Amz-Signature=c7c126f5b9940ad3cd9f82ab14876fda422554524da26c8c9173f233962311e0&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22svg-2.svg%22&x-id=GetObject",
   alt: 'Music',
   darkText: false,
 };
@@ -48,7 +46,7 @@ export const homeObjThree = {
      can hear them whenever he wants.`,
   buttonLabel: 'Get started',
   imgStart: false,
-  img: img3,
+  img: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/2e926179-471a-45d9-9afe-aa95c170f0f9/svg-3.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211129%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211129T195618Z&X-Amz-Expires=86400&X-Amz-Signature=ea37c6ea18c42d7483368cc463271faefd9fb348ae2194d7f55920a45e769940&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22svg-3.svg%22&x-id=GetObject",
   alt: 'Music',
   darkText: true,
 };
