83f4813d233fccab8532c89348791658d42a1ae1
Henrique <hentt30@gmail.com>
2021-11-29 16:24:24 -0300

    removendo conflitos

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,11 +12,11 @@ createConnection()
     .then(async (connection) => {
     // Create a new express application instance
       const app = express();
-      app.use(express.static(path.join(__dirname, './client/build')));
+      app.use(express.static(path.join(__dirname, '../../client/build')));
 
 
       app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, './client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../../client', 'build', 'index.html'));
       });
 
 
