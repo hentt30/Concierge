89d907a7dc8426be0013d0407a3b16b1306bfd07
Henrique <hentt30@gmail.com>
2021-11-29 21:56:00 -0300

    Entidaded

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -4,7 +4,7 @@ createConnection(
     {
     url: process.env.DATABASE_URL,
     type: 'postgres',
-    entities: [ "{dist,src}/entity/*{.js,.ts}"]['YOUR ENTITIES GO HERE'],
+    entities: [ "{dist,src}/entity/*{.js,.ts}"],
     synchronize: true,
     extra: {
       ssl: true,
